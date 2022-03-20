6624c70473db5cfdfc41c6c648f827133c84c54f
jadenlohh <jadenloh24@gmail.com>
2022-03-20 15:43:51 +0800

    Fixed errors

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
-const express = require('express')
-const path = require('path')
-const { MongoClient }  = require('mongodb')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv').config()
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const { MongoClient }  = require("mongodb");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const client = new MongoClient(process.env.mongouri, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", require("./authentication"));
+app.set("view engine", "ejs");
+app.use(cookieParser());
 
 
-const app = express()
-const client = new MongoClient(process.env.mongouri, { useNewUrlParser: true, useUnifiedTopology: true })
-app.use(express.static('public'))
-app.set('view engine', 'ejs')
-app.use('/', require('./authentication'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.get("/", (req, res) => {
+    res.redirect("/login");
+});
 
 
-app.get('/', (req, res) => {
-    res.redirect('/login')
-})
-
-
-app.get('/dashboard', (req, res) => {
+app.get("/dashboard", (req, res) => {
     try {
-        if (!req.cookies.token) throw err
+        if (!req.cookies.token) throw err;
 
-        jwt.verify(req.cookies.token, 'secret', (err, payload) => {
+        jwt.verify(req.cookies.token, "secret", (err, payload) => {
             client.connect(err => {
-                const collection = client.db("passkeeper").collection("credentials")
+                const collection = client.db("passkeeper").collection("credentials");
 
-                collection.findOne({'email': payload.email}, (err, account) => {
-                    res.render('dashboard', { 'firstName': account.firstName, 'lastName': account.lastName, 'email': account.email })
-                })
-            })
-        })
+                collection.findOne({"email": payload.email}, (err, account) => {
+                    res.render("dashboard", { "firstName": account.firstName, "lastName": account.lastName, "email": account.email });
+                });
+            });
+        });
     }
     catch (err) {
-        res.redirect('/login')
+        res.redirect("/login");
     }
-})
+});
+
+
+app.post("/dashboard", (req, res) => {
+    client.connect(err => {
+        const collection = client.db("passkeeper").collection("credentials");
+        var updateCredentials = { $set: {"firstName": req.body.firstName, "lastName": req.body.lastName} };
+
+        jwt.verify(req.cookies.token, "secret", (err, payload) => {
+            collection.updateOne({"email": payload.email}, updateCredentials);
+
+            collection.findOne({"email": payload.email}, (err, result) => {
+                res.render("dashboard", { "firstName": result.firstName, "lastName": result.lastName, "email": result.email, "updatedCredentials": true });
+            });
+        });
+    });
+});
+
+
+app.get("logout", (req, res) => {
+    res.clearCookie("token");
+    res.redirect("/login");
+});
 
 
 app.listen(3000, () => {
-    console.log('App listening on port 3000')
-})
+    console.log("App listening on port 3000");
+});
--- authentication.js
+++ authentication.js
@@ -1,92 +1,83 @@
-const express = require('express')
-const { MongoClient }  = require('mongodb')
-const bcrypt = require('bcrypt')
-const path = require('path')
-const ejs = require('ejs')
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv').config()
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
-const authentication = express.Router()
-const client = new MongoClient(process.env.mongouri, { useNewUrlParser: true, useUnifiedTopology: true })
-authentication.use(bodyParser.urlencoded({ extended: false }))
-authentication.use(cookieParser())
+const router = express.Router();
+const client = new MongoClient(process.env.mongouri, { useNewUrlParser: true, useUnifiedTopology: true });
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(cookieParser());
 
 
-
-authentication.get('/login', (req, res) => {
-    res.render('login', { 'failedAuth': false })
-})
-
-
-authentication.post('/login', (req, res) => {
-    client.connect(err => {
-        const collection = client.db("passkeeper").collection("credentials")
-
-        collection.findOne({'email': req.body.email}, (err, account) => {
-            bcrypt.compare(req.body.password, account.password, (err, validPwd) => {
-                if (validPwd) {
-                    var token = jwt.sign({'email': req.body.email}, 'secret', {expiresIn: '3d'})
-                    
-                    res.cookie('token', token)
-                    res.redirect('/dashboard')
-                }
-                else {
-                    res.render('login', { 'failedAuth': true, 'email': req.body.email })
-                }
-            })
-        })
-    })
-})
+router.get("/login", (req, res) => {
+    res.render("login");
+});
 
 
-authentication.get('/register', (req, res) => {
-    res.render('registration', { 'emailAlreadyExist': false })
-})
+router.post("/login", (req, res) => {
+    client.connect((err) => {
+        const collection = client.db("passkeeper").collection("credentials");
+
+        collection.findOne({ "email": req.body.email }, (err, account) => {
+            if (account) {
+                bcrypt.compare(req.body.password, account.password, (err, validPwd) => {
+                    if (validPwd) {
+                        var token = jwt.sign({ "email": req.body.email }, "secret", { expiresIn: "3d" });
+    
+                        res.cookie("token", token);
+                        res.redirect("/dashboard");
+                    } 
+                    else {
+                        res.render("login", { "error": 'InvalidCredentials', "email": req.body.email });
+                    }
+                });
+            }
+            else {
+                res.render("login", { "error": 'InvalidCredentials', "email": req.body.email });
+            }
+        });
+    });
+});
 
 
-authentication.post('/register', (req, res) => {
-    client.connect(err => {
-        var collection = client.db("passkeeper").collection("credentials")
+router.get("/register", (req, res) => {
+    res.render("registration");
+});
 
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(req.body.password, salt, (err, hash) => {
-                var credentials = {
-                    'firstName': req.body.firstName,
-                    'lastName': req.body.lastName,
-                    'email': req.body.email,
-                    'password': hash,
-                    'twoFactorAuth': false
-                }
-                collection.insertOne(credentials)
-            })
-        })
 
-    //     var account = collection.findOne({'email': req.body.email})
+router.post("/register", (req, res) => {
+    client.connect((err) => {
+        const collection = client.db("passkeeper").collection("credentials");
+        var { firstName, lastName, email, password} = req.body;
 
-    //     if (account) {
-    //         res.render('registration', { 'emailAlreadyExist': true, 'firstName': req.body.firstName, 'lastName': req.body.lastName, 'email': req.body.email }) 
-    //     }
-        
-    //     bcrypt.genSalt(10, (err, salt) => {
-    //         bcrypt.hash(req.body.password, salt, (err, hash) => {
-    //             var credentials = {
-    //                 'firstName': req.body.firstName,
-    //                 'lastName': req.body.lastName,
-    //                 'email': req.body.email,
-    //                 'password': hash,
-    //                 'twoFactorAuth': false
-    //             }
-    //             collection.insertOne(credentials, () => { client.close() })
-    //             res.redirect('/')
+        var account = collection.findOne({ "email": req.body.email });
 
-    //             var token = jwt.sign({'email': req.body.email}, 'secret', {expiresIn: '60'})
-    //             document.cookie = 'token=' + token + ';'
-    //             res.redirect('/dashboard')
-    //         })
-    //     })
-    })
-})
+        if (account) {
+            res.render("registration", { "error": "AccountAlreadyExist", "firstName": firstName, "lastName": lastName, "email": email });
+        }
+        else {
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(password, salt, (err, hash) => {
+                    var credentials = {
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: email,
+                        password: hash,
+                        twoFactorAuth: false,
+                    };
+    
+                    collection.insertOne(credentials);
+                });
+            });
 
-module.exports = authentication
+            res.cookie("token", jwt.sign({ "email": req.body.email }, "secret", { expiresIn: "3d" }));
+            res.redirect("/dashboard");
+        };
+    });
+});
+
+
+module.exports = router;
